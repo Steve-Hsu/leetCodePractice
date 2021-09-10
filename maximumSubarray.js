4e51a5877e9e76634a0c6e296b254aa17b48c7d9
Steve-Hsu <wuujdsc@gmail.com>
2021-09-10 14:33:37 +0700

    maximum Subarray - more compact

--- maximumSubarray.js
+++ maximumSubarray.js
@@ -5,23 +5,15 @@
 
 // My Solution_1
 var maxSubArray = function (nums) {
-  let currentMax = nums[0];
+  let curMax = nums[0];
   let sum = 0
 
-  for (item of nums) {
-    if (sum < 0) {
-      sum = item
-    } else {
-      sum = sum + item
-    }
-    if (currentMax < item) {
-      currentMax = item
-    }
-    if (currentMax < sum) {
-      currentMax = sum
-    }
+  for (n of nums) {
+    sum = sum < 0 ? n : sum + n;
+    if (curMax < n) curMax = n;
+    if (curMax < sum) curMax = sum;
   }
-  return currentMax
+  return curMax
 };
 
 let nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
