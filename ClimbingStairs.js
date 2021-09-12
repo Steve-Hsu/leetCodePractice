783efa43b3da420bd87720640d97862e5240e363
Steve-Hsu <wuujdsc@gmail.com>
2021-09-12 12:56:00 +0700

    climb Stairs, - recursion with memo

--- ClimbingStairs.js
+++ ClimbingStairs.js
@@ -16,7 +16,7 @@ const addUpTo = (n) => {
 
 // console.log(addUpTo(test))
 
-// Lootcode solution_1, Brute Force 
+// Leetcode solution 1, Brute Force 
 const bruteForceClimbStairs = (n) => {
   const climb_Stairs = (i, n) => {
     // i defines the current step
@@ -28,5 +28,21 @@ const bruteForceClimbStairs = (n) => {
   return climb_Stairs(0, n)
 };
 
-let N = 3
-console.log("Brute force", bruteForceClimbStairs(N));
+
+// console.log("Brute force", bruteForceClimbStairs(N));
+let N = 5
+
+// LeetCode solution 2: Recursion with Memoization
+const memoClimbStairs = (n, memo = []) => {
+  const climb_Stairs = (i, n) => {
+    // i defines the current step
+    // n defines the destination step.
+    if (i > n) return 0;
+    if (i === n) return 1;
+    if (memo[i] > 0) return memo[i]
+    memo[i] = climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
+    return memo[i]
+  }
+  return climb_Stairs(0, n)
+}
+console.log("Recursion with memo", memoClimbStairs(N));
