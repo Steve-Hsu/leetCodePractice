574d9478500188f33015a1ebafa6f1bfd8b26e9a
Steve-Hsu <wuujdsc@gmail.com>
2021-09-19 12:03:11 +0800

    module binary tree maker

---
+++ binaryTreeMaker.js
@@ -0,0 +1,33 @@
+function TreeNode(val, left, right) {
+  this.val = (val === undefined ? 0 : val)
+  this.left = (left === undefined ? null : left)
+  this.right = (right === undefined ? null : right)
+};
+
+module.exports = makeTreeWithArray = (arr) => {
+  let arrLength = arr.length;
+  let root = new TreeNode();
+  let nodes = [root];
+  let level = 0;
+  let currentRoot = nodes[level];
+  let leftIdx = -1
+  let rightIdx = 0
+  for (let i = 0; i <= arrLength - 1; i++) {
+    if (arr[i]) {
+      currentRoot = nodes[level];
+      leftIdx = leftIdx + 2
+      rightIdx = rightIdx + 2
+      currentRoot.val = arr[i];
+      if (arr[leftIdx]) {
+        currentRoot.left = new TreeNode(arr[leftIdx]);
+        nodes.push(currentRoot.left);
+      }
+      if (arr[rightIdx]) {
+        currentRoot.right = new TreeNode(arr[rightIdx]);
+        nodes.push(currentRoot.right);
+      }
+      level = level + 1;
+    }
+    if (i == arrLength - 1) return root
+  }
+};
